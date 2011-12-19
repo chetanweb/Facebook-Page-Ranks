a4a8bc42a8eff0c45ce53d3e8bd2903dcb60682f
chetanweb <me@chetanweb.com>
2011-12-20 00:18:10 +0530

    Removed application id and secret key

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
-$appid='161861570581951';
-$secret='4a2209292e97d60fafa6f9848723f641';
+$appid='';
+$secret='';
 
-$pageid='http://google.com';
 $source_ids="107329506051213";
 $source_name="Nagpur PHP Users Group";
 ?>
